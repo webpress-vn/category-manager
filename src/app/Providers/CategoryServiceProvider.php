c1b1bd03befa32c1e682742616d9456a8ec38b89
QuangHuy <huybq@vicoders.com>
2020-07-16 15:11:09 +0700

    update composer

--- src/app/Providers/CategoryServiceProvider.php
+++ src/app/Providers/CategoryServiceProvider.php
@@ -25,10 +25,6 @@ class CategoryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../../migrations/' => database_path('migrations'),
-        ], 'migrations');
-
         $this->loadMigrationsFrom(__DIR__ . '/../../migrations');
 
         $this->publishes([
