0f30184611c4fd69fe5c4044043964623da8a2e2
QuangHuy <huybq@vicoders.com>
2020-07-18 08:24:29 +0700

    edit provider category

--- src/app/Providers/CategoryServiceProvider.php
+++ src/app/Providers/CategoryServiceProvider.php
@@ -24,10 +24,6 @@ class CategoryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../../migrations/' => database_path('migrations'),
-        ], 'migrations');
-
         $this->loadMigrationsFrom(__DIR__ . '/../../migrations');
 
         $this->publishes([
