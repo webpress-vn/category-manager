4a6f68ed63ce98ad0cd0fc95388521efe5d19561
vietanhxan <vietanhxan@gmail.com>
2020-11-09 15:30:26 +0700

    rename column hot to is_hot in table category

---
+++ src/migrations/2020_11_09_082523_alter_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->renameColumn('hot', 'is_hot');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            if (Schema::hasColumn('is_hot')) {
+                $table->renameColumn('is_hot', 'hot');
+            }
+        });
+    }
+}
